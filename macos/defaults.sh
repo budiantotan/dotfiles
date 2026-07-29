d3b14a6fff9475419e4e9f1bff1aedd6c4cb0bc3
Budianto Tan <22911816+budiantotan@users.noreply.github.com>
2026-07-29 22:25:49 +0800

    feat: add macos defaults & functions

---
+++ macos/defaults.sh
@@ -0,0 +1,276 @@
+# Inspired from https://mths.be/macos & https://macos-defaults.com
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Set liquid glass to tinted
+defaults write -g NSGlassDiffusionSetting -int 1
+
+# Enable dragging window with cmd key
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
+# Set sidebar icon size to medium
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+# Set Show scrollbars to automatic
+defaults write NSGlobalDomain AppleShowScrollBars -string "Automatic"
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+# Disable Resume system-wide
+defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -bool false
+
+# Disable Notification Center and remove the menu bar icon
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+
+# Disable automatic capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+
+#  Remove items in the bin after 30 days
+defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+# Dock: Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Dock: Set the icon size of magnification to 48 pixels
+defaults write com.apple.dock largesize -int 48
+
+# Enable dock magnification
+defaults write com.apple.dock magnification -bool true
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Set Always Auto hide
+defaults write com.apple.dock "autohide" -bool true
+
+# Disable grouping windows by application
+defaults write com.apple.dock "expose-group-apps" -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+###############################################################################
+# Menubar                                                                     #
+###############################################################################
+
+# Date format 
+defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM HH:mm"
+
+# Always hide the menu bar
+defaults write -g _HIHideMenuBar -int 1
+
+###############################################################################
+# Keyboard, trackpad, mouse                                                   #
+###############################################################################
+
+# Trackpad: enable tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Trackpad: 2 finger tap right click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -bool true
+
+# Keyboard: Fn key triggers emoji 
+defaults write com.apple.HIToolbox AppleFnUsageType -int 2
+
+# Keyboard: Set default function keys behavior (1 = Use F1, F2 etc. as standard keys)
+defaults write -g "com.apple.keyboard.fnState" -int 1
+
+# Keyboard: Enable moving focus with Tab and Shift Tab.
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
+
+###############################################################################
+# Energy configurations                                                       #
+###############################################################################
+
+# Disable machine sleep while charging
+sudo pmset -c displaysleep 0 sleep 0
+
+# Set machine sleep to 5 minutes on battery
+sudo pmset -b displaysleep 10 sleep 15
+
+###############################################################################
+# Spotlight                                                                   #
+###############################################################################
+
+# Disable indexing
+sudo mdutil -i off / > /dev/null
+# Erase the current index
+sudo mdutil -E / > /dev/null
+
+###############################################################################
+# Terminal                                                                    #
+###############################################################################
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+# Enable Secure Keyboard Entry in Terminal.app
+defaults write com.apple.terminal SecureKeyboardEntry -bool true
+
+# Disable the annoying line marks
+defaults write com.apple.Terminal ShowLineMarks -int 0
+
+###############################################################################
+# Activity Monitor                                                            #
+###############################################################################
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+###############################################################################
+# Software updates                                                            #
+###############################################################################
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+###############################################################################
+# Window manager (Stage manager)                                                                      #
+###############################################################################
+
+# Disable desktop click
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
+
+# Disable stage manager
+defaults write com.apple.WindowManager GloballyEnabled -bool false
+
+# Disable widgets on desktop
+defaults write com.apple.WindowManager StandardHideWidgets -bool true
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+echo "Restarting affected macOS services..."
+
+for app in \
+    "Activity Monitor" \
+    "Dock" \
+    "cfprefsd" \
+    "Finder" \
+    "Photos" \
+    "SystemUIServer" \
+    "Terminal" \
+    "WindowManager"
+do
+    killall "$app" >/dev/null 2>&1 || true
+done
---
+++ scripts/macos.sh
@@ -0,0 +1,34 @@
+setup_xcode_select() {
+    if ! xcode-select -p >/dev/null 2>&1; then
+        echo "⚙️ Installing Xcode Command Line Tools..."
+
+        xcode-select --install
+
+        echo "⏳ Waiting for installation to complete..."
+
+        while ! xcode-select -p >/dev/null 2>&1; do
+            read -rp "⏳ Press ENTER once the installation has finished..."
+        done
+
+        echo "✅ Xcode Command Line Tools installed."
+    fi
+
+    echo "✅ Xcode Command Line Tools already installed. Skipping."
+}
+
+apply_macos_defaults() {
+    local defaults_script="$ROOT/macos/defaults.sh"
+
+    if [[ -f "$defaults_script" ]]; then
+        echo "⚙️ Applying macOS defaults..."
+        bash "$defaults_script"
+
+        echo "✅ macOS defaults applied."
+
+        return
+    fi
+
+    echo "❌ Error: macOS defaults not found. Skipping."
+}
+
+# add discover defaults script here
